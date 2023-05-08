936bb37ccf68452f0f8fcc00877f650d16dd1f23
DartSter <kil-all@rambler.ru>
2023-05-08 15:08:53 +0300

    feat: copy files function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const path = require('node:path');
+const { copyFile, mkdir, rm } = require('node:fs/promises');
+const { readdir } = require('node:fs/promises');
+const newFolderPath = path.resolve(__dirname, 'files-copy');
 
+mkdir(newFolderPath, { recursive: true })
+  .then(() => rm(newFolderPath, { recursive: true }))
+  .then(() => mkdir(newFolderPath, { recursive: true }))
+  .then(() => readdir(path.resolve(__dirname, 'files')))
+  .then((data) =>
+    data.map((item) => {
+      copyFile(
+        path.resolve(__dirname, 'files', item),
+        path.resolve(newFolderPath, item)
+      );
+    })
+  );
